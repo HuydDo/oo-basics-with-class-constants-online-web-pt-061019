5b52c27bb613347783d6cebad22e4c15a2027da9
Huy Do <huyddo40@gmail.com>
2019-06-17 05:04:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -16,4 +16,13 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+<<<<<<< HEAD
+=======
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+
+  BRANDS.uniq
+>>>>>>> 0239558de84ddeddf77cf0d69b3c9883c37c7e41
 end
